3575acc471e05baff4174b8a45270b3be81ff63f
Keith Owens <kowens@TMRIUSA.COM>
2016-05-25 11:02:07 -0500

    Manual merge from keithowens.github.io

--- js/helper.js
+++ js/helper.js
@@ -14,48 +14,50 @@ replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderpRole = '<p>%data%</p><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="gray-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="gray-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="gray-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="gray-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="gray-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="gray-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+var HTMLpwelcomeMsg = '<p class="welcome-message">%data%</p>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskillsStart = '<hr><h3 id="skills-h3">Skills Summary:</h3><ul id="skills" class="flex-box"></ul>';
+var HTMLskills = '<li class="flex-item"><span class="gray-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
 var HTMLworkTitle = ' - %data%</a>';
-var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkDates = '<div class="date-text">%data%</div><br>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLworkDescription = '<p>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
-var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectDates = '<div class="date-text">%data%</div><br>';
+var HTMLprojectDescription = '<p>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
-var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolName = '<a href=%url%>%data%';
+var HTMLschoolDegree = ' -- %data%</em>';
+var HTMLschoolDates = '<div class="date-text">%data%</div><br>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolMajor = '<em>Major: %data%';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineClasses = '<h3 id="classes-h3">Online Classes</h3>';
+var HTMLonlineTitle = '<a href=%url%>%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
-var internationalizeButton = '<button>Internationalize</button>';
+var internationalizeButton = '<button class = "internationalize">Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
 /*
@@ -247,11 +249,12 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap, true);  //Third argument needed to prevent Firefox exception
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
 window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
   map.fitBounds(mapBounds);
-});
+},true);  //Third argument needed to prevent Firefox exception
+
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,17 +10,26 @@ if (!String.prototype.trim)
  
  var bio = {
 			"name" : "Keith Owens",
-			"role" : "Software Engineer/Web Developer",
+			"role" : "Software Engineer, Embedded and Web",
 			"contacts" : {
 						  "mobile" : "815-353-6949",
 						  "email" : "koengineer@ymail",
-						  "github" : "KeithOwens",
+						  "github" : "<span class=\"gray-text\" href = \"https://github.com/KeithOwens\">KeithOwens</span>",
 						  "location" : "Crystal Lake, IL"
 						 },
 			"biopic" : "images/KO_Closeup.JPG",
-			"welcomeMessage" : "Thanks for viewing my online resume'.  You are looking at your next best employee.",
-			"skills" : ["programming", "hardware design", "debugging", "unit and integration testing", "Six-sigma Green Belt"]
-			};
+			"welcomeMessage" : "<hr>Thanks for viewing my online resume'.  Please contact me, so we can talk about how I can improve your company's bottom line.",
+			"skills" : ["Software Development - Multi-processor, multi-threading, algorithms, System V and POSIX APIs, MFC, .NET, object-oriented, user and kernel space",
+					"Languages - C, C++, C#, HTML, CSS, Javascript, Java, assembly",
+					"Operating Systems  - Linux, Windows, QNX, VxWorks",
+					"Version Control Tools - SVN, Git, Clearcase",
+					"Defect Tracking - Trac, Clearquest",
+					"Debugging - Single-step, trace, logging", 
+					"Testing - Unit, integration, regression, automated, static analysis", 
+					"Technical Management - Multi-discipline project schedules and priorities, FMEAs, FOSS",
+					"Systems, hardware, and software requirements, design, and interface doucment preparation",
+					"Hardware design - digital, audio, power management, EMI, ESD, UL, I/O, DFM, Quality"]
+            };
 				 
 var work = { 
 			"jobs" : [
@@ -28,47 +37,45 @@ var work = {
 						"employer" : "Toshiba Medical Research Institute USA",
 						"title" : "Principal Software Engineer",
 						"location" : "Vernon Hills, IL",
-						"dates" : "July, 2012 - Present",
-						"description" : "Design, implement, and maintain data acquisition, system health monitoring/reporting, "+
-								"and detector calibration.  "+  "Responsible for developing automated testing as part of "+
-								"task design and implementation.  "+"Responsible for FOSS compliance.  Responsible for "+
+						"dates" : "Jul, 2012 - Present",
+						"description" : "Staff-level, individual contributor completely responsible for design, implementation, and maintenance of "+
+								"data acquisition, system health monitoring/reporting, and detector calibration.  "+  
+								"Responsible for developing automated testing as part of "+
+								"task design and implementation.  Responsible for FOSS compliance.  Responsible for "+
 								"helping improve software quality, including maintenance of Coverity static analysis tool."
 					   },
 					   {
 						"employer" : "Toshiba Medical Research Institute USA",
 						"title" : "Principal Software Engineer",
 						"location" : "Vernon Hills, IL",
-						"dates" : "March, 2007 - July, 2012",
-						"description" : "Design and implement new features for and maintain image-processing subsystem.  "+
-								"Responsible for creating a system-integration process and release test plan.  "+
-								"Responsible for directing team in feasibility study for next-generation cardiac, "+
+						"dates" : "Mar, 2007 - Jul, 2012",
+						"description" : "Staff-level, individual contributor completely design and implement new features for and maintenance of the "+
+								"image-processing subsystem.  Responsible for creating a system-integration process and release test plan.  "+
+								"Responsible for techincal leadership of X-ray software team.  Led group in feasibility study for next-generation cardiac, "+
 								"angiographic, and neurological platform X-ray system."
 					   },
 					   {
 						"employer" : "Motorola",
 						"title" : "Principal Component Sourcing Engineer",
 						"location" : "Libertyville, IL",
-						"dates" : "November, 2004 - March, 2007",
-						"description" : "Primarily responsible for qualifying and acquiring production quantities of new components"+
-								"for production of new 3G phones.  Related activities included working with "+
-								"vendors to gain competitive production pricing for new components, working with vendors "+
-								"for compliance with Motorola components specifications, working with the prototype factory "+
-								"to identify any build-related issues with new components, and working with Category "+
-								"Management to identify cost reduction opportunities related to phone BOMs."
+						"dates" : "Nov, 2004 - Mar, 2007",
+						"description" : "Staff-level, individual contributor responsible for all product-level qualification and sourcing of production quantities of new components "+
+								"for new 3G phones.  Required coordinating activities with Category Management, prototype manufacturing, suppliers, and Development engineering "+
+								"including the identification of cost reduction opportunities related to phone BOMs."
 					   },
 					   {
 						"employer" : "Motorola",
 						"title" : "Principal Software Engineer",
 						"location" : "Deer Park, IL",
-						"dates" : "November, 2001 - November, 2004",
-						"description" : "Developed algorithms and C implementations for the full-duplex hands-free software module."+
+						"dates" : "Nov, 2001 - Nov, 2004",
+						"description" : "Staff-level, individual contributor who developed algorithms and C implementations for the full-duplex hands-free software module."+
 								"  Responsible for mentoring and directing work tasks for two junior engineers in Deer Park office."
 					   },
 					   {
 						"employer" : "Motorola",
 						"title" : "Senior Hardware Engineer",
 						"location" : "Boynton Beach, FL",
-						"dates" : "January, 1994 - November, 2001",
+						"dates" : "Jan, 1994 - Nov, 2001",
 						"description" : "Led design of baseband sections for mobile radios, pagers, GSM mobile phone, and "+
 										"CDMA Telematics embedded telephone."
 					   }
@@ -80,38 +87,38 @@ var projects = {
 							   {
 								"title" : "PET Scanner",
 								"dates" : "2012 - Present",
-								"description" : "<li>Gated Acquisition - Design, implement, and now maintain the new respiratory gating "+
-								"waveform and trigger information Collected from an Anzai device.</li>"+
-								"<li>Data Acquisition - Design, implement, and now maintain the data collection tasks on the Acquisition Server.  "+
-								"Included creation of a scan hardware simulator and data verification test program.</li>"+
+								"description" : "<li>Gated Acquisition - Design, implement, and maintain the new respiratory gating "+
+								"waveform and trigger information collected for patient studies. (C, C++, Linux, SVN)</li>"+
+								"<li>DICOM Study Converter - Create conversion program for research groups to convert DICOM studies to proprietary data "+
+								"formats. (C, C++, Windows, MFC, SVN)</li>"+
+								"<li>Data Acquisition - Design, implement, and maintain the data collection tasks on the Acquisition Server.  "+
+								"Included creation of a scan hardware simulator and data verification test program. (C, C++, Linux, QNX, Windows, C#, .NET, SVN, FMEA) </li>"+
+								"<li>Automated Testing - Update and extend overnight automated tests to handle 40 different acquisition test cases.  "+
+								"(C#, .NET, Windows, SVN)</li>"+
 								"<li>Detector Calibration - Port research software/algorithms to run on production system.  Includes interfacing to "+
-								"new Host server software and calculation speed-ups.</li>"+
+								"new Host server software and calculation speed-ups. (C, C++, MATLAB, Windows, SVN)</li>"+
 								"<li>Health Monitoring  - Added new features to automated testing to better model production system "+
-								"performance and eliminate system-level defects.</li>"+
-								"<li>Version Validation - Added new features to confirm correct SW versions are running at system start-up.</li>"+
-								"<li>Release Versioning - Separated release software into versions, which do not change often, and versions that "+
-								"are likely to change with every release.</li>",
-								
-								"images" : ["images/Toshiba-PET-CT.jpg", "images/celesteion01.jpg"]
+								"performance and eliminate system-level defects. (C, C++, Linux, Windows, C#, .NET, SVN)</li>",
+								"images" : ["images/Toshiba-PET-CT.jpg", "images/celesteion_mod.jpg"]
 							   },
 							   {
 								"title" : "X-ray Angiography/Cardiac Scanner",
 								"dates" : "2007 - 2012",
 								"description" : "<li>Next-generation X-ray system - Proved feasibility of Preempt-RT Linux OS for X-ray/data "+
-								"acquisition applications as part of a cost-reduction effort to use COTS hardware.</li>"+
+								"acquisition applications as part of a cost-reduction effort to use COTS hardware. (C, C++, Linux, OpenGL/GLX, SVN)</li>"+
 								"<li>High-speed data transfer for VCT - Implemented Fibre Channel communication between RAID and separate "+
-								"cariac-application server to support new Cardiac applications.</li>"+
+								"cariac-application server to support new Cardiac applications. (C, C++, VxWorks, C#, .NET, SVN)</li>"+
 								"<li>Cost Reduction - Reduce cost by updating older product boards to newer parts and reducing the number of "+
-								"image-processing boards needed.</li>",
+								"image-processing boards needed. (C, C++, VxWorks, MFC, Windows, SVN)</li>",
 								"images" : ["images/cardiac_p8000v.jpg", "images/panel-angiography.jpg"]
 							   },
 							   {
 								"title" : "3G Mobile Phone Component Sourcing (Supply Chain)",
 								"dates" : "2004 - 2007",
 								"description" : "<li>Cost Reduction - Identify second- and third-source suppliers for new and high-volume "+
-								"existing components.  Explain component pricing and BOM structure to Business Devleopment teams to promote "+
-								"cost reduction and accurate phone cost estimates for new phone business plans.</li>"+
-								"<li>IC technology Roadmap - Created dual-sourced, 3-year memory roadmap for stacked memory for 3 new 3G phones.</li>"+		
+								"existing components.  Explain component pricing and BOM structure to Business Development teams to promote "+
+								"cost reduction and accurate phone cost estimates for new phone business plans.(Project schedules)</li>"+
+								"<li>IC technology Roadmap - Created dual-sourced, 3-year memory roadmap for stacked memory for 3 new 3G phones. (Project schedules, priorities, risk mitigation)</li>"+		
 								"<li>BOM preparation  - Work with new product development teams, factories, and Category Management on transition to "+
 								"new BOM entry/control system.</li>"+
 								"<li>Green Belt - Earned Six-Sigma Green Belt to improve component quality.</li>",
@@ -122,29 +129,28 @@ var projects = {
 								"dates" : "2001 - 2004",
 								"description" : "<li>Voice Activity Detector - Designed, developed, and tested for production a new voice activity "+
 								"detector, echo estimator, and landline echo canceller for the hands-free software solution resulted in a $6/unit savings "+
-								"for products shipping 1million+ units/year.</li>"+
+								"for products shipping 1 million+ units/year.(C, MATLAB, project schedules, Clearcase, Clearquest)</li>"+
 								"<li>Deep Cost Reduction - Involved in the system, hardware, and software specification and initial design of a "+
-								"Telematics support gate array for executing current and future hands-free, voice recognition, and navigation applications.</li>"+
+								"Telematics support gate array for executing current and future hands-free, voice recognition, and navigation applications. (C)</li>"+
 								"<li>Quality Initiative - Successfully led the management effort within the Hands-free Software team to achieve"+
-								" SEI Level 3 to secure new contracts with existing automotive customers.</li>",
+								" SEI Level 3 to secure new contracts with existing automotive customers.  (Documentation, Clearcase, Clearquest)</li>",
 								"images" : ["images/caraudio.jpg", "images/echocancellerpic.jpg"]
 							   },
 							   {
 								"title" : "Baseband Design for Various Wireless Products",
 								"dates" : "1994 - 2001",
-								"description" : "<li>Digital Embedded Telephone - Designed and implemented Qualcomm-baseed CDMA embedded mobile phone "+
+								"description" : "<li>Digital Embedded Telephone - Designed and implemented Qualcomm-based CDMA embedded mobile phone "+
 								"for use with GM-OnStar products.  Evaluated performance of Qualcomm, Conexant, and Infineon mobile phone chipsets for "+
-								"use in future embedded mobile phone products.</li>"+
+								"use in future embedded mobile phone products. (Digital, power management, EMI, ESD, I/O, FMEA, DFM, quality)</li>"+
 								"<li>Wireless Products - Designed, implemented, and tested the digital controller, power, audio, lighting, and "+
-								"alert sections of public safety radios, numeric and alphanumeric pagers, and a GSM phone.</li>"+
+								"alert sections of public safety radios, numeric and alphanumeric pagers, and a GSM phone.(digital, audio, power management, UL, ESD, EMI, I/O, DFM, quality)</li>"+
 								"<li>Cost Reduction - Successfully identified, implemented, and tested several circuit redesigns on multiple pagers.  "+
 								"Design changes on an alphanumeric Pager resulted in a cost savings of $5/unit for a product that shipped "+
-								"another 2 million units until EOL.</li>"+
+								"another 2 million units until EOL.  (digital, audio, power management, ESD, EMI, I/O, DFM, quality)</li>"+
 								"<li>Quality Improvements - Successfully collaborated with factory and field service teams on various product "+
 								"redesigns to reduce manufacturing defects, simplify product servicing, and resolve customer return issues.  "+
-								"Modified pager circuitry to pass ESD and FCC emissions tests and prepared FCC and UL approval design documents "+
-								"for several pager products.</li>",
-								"images" : ["images/MCS2000 MDL 2.jpg", "images/advisor gold flx.jpg", "images/mo-cd930-blau-1-small.jpg"]
+								"(digital, audio, power management, EMI, ESD, UL, I/O, DFM, Quality)</li>",
+								"images" : ["images/devices.jpg"]
 							   }
 							]
 				};
@@ -163,14 +169,14 @@ var education ={
 							  "degree" : "Masters",
 							  "majors" : ["EE, DSP and Digital comm specializations"],
 							  "dates" : "1994 - 1998",
-							  "url" : "http://www.ece.ufl.edu/"
+							  "url" : "http://www.ece.ufl.edu"
 							  },
 							  {"name" : "University of Florida",
 							  "location" : "Gainesville, FL",
 							  "degree" : "BS",
 							  "majors" : ["EE, Mathematics minor"],
 							  "dates" : "1988 - 1993",
-							  "url" : "http://www.ece.ufl.edu/"
+							  "url" : "http://www.ece.ufl.edu"
 							  }
 							],
 				"onlineCourses" : [
@@ -237,22 +243,30 @@ bio.display = function()
 	{
 		var formattedName        = HTMLheaderName.replace("%data%", bio.name);
 		var formattedRole        = HTMLheaderRole.replace("%data%", bio.role);
+		var formattedpRole       = HTMLheaderpRole.replace("%data%", bio.role);
 		var formattedMobile      = HTMLmobile.replace("%data%", bio.contacts.mobile);
 		var formattedEmail       = HTMLemail.replace("%data%", bio.contacts.email);
 		var formattedGitHub      = HTMLgithub.replace("%data%", bio.contacts.github);
 		var formattedLocation    = HTMLlocation.replace("%data%", bio.contacts.location);
 		var formattedBioPic      = HTMLbioPic.replace("%data%", bio.biopic);
 		var formattedWelcomeMsg  = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+		var formattedpWelcomeMsg = HTMLpwelcomeMsg.replace("%data%", bio.welcomeMessage);
 	 
-		$("#header").prepend(formattedRole);
+		if ($(window).width() > 600)
+		{
+			$("#header").prepend(formattedRole);
+		}
+		else
+		{
+			$("#header").prepend(formattedpRole);
+		}
 		$("#header").prepend(formattedName);
+		$("#header").prepend(formattedBioPic);
+		$("#topContacts").append(formattedLocation);
 		$("#topContacts").append(formattedMobile);
 		$("#topContacts").append(formattedEmail);
 		$("#topContacts").append(formattedGitHub);
-		$("#topContacts").append(formattedLocation);
-		$("#header").append(formattedBioPic);
-		$("#header").append(formattedWelcomeMsg);
- 
+		//$("#header").append(formattedBioPic);
 		if (bio.skills.length > 0)		 
 		{
 			 $("#header").append(HTMLskillsStart);
@@ -261,6 +275,10 @@ bio.display = function()
 				var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
 				$("#header").append(formattedSkills);
 			 }
+		}
+		if ($(window).width() > 767)
+		{
+			$("#header").append(formattedWelcomeMsg);
 		}
 	}
 }
@@ -304,7 +322,7 @@ projects.display = function()
 			$(".project-entry:last").append(formattedProjDates);
 			$(".project-entry:last").append(formattedProjDescription);
 			
-			if ($(window).width() > 600)
+			if ($(window).width() > 767)
 			{
 				if (projects.project[project].images.length > 0)
 				{
@@ -328,6 +346,7 @@ education.display = function()
 			$("#education").append(HTMLschoolStart);
 			
 			var formattedschoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+			formattedschoolName = formattedschoolName.replace("%url%", education.schools[school].url);
 			var formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
 			var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
 			var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
@@ -336,7 +355,7 @@ education.display = function()
 			if (education.schools[school].hasOwnProperty('location'))
 			{
 				var HTMLschoolLocation = HTMLworkLocation.replace("%data%", education.schools[school].location);
-				$(".education-entry:last").append(HTMLschoolLocation);
+				$(".education-entry:last").prepend(HTMLschoolLocation);
 			}
 			$(".education-entry:last").append(formattedschoolDates);
 			$(".education-entry:last").append(formattedschoolMajor);	
@@ -351,17 +370,17 @@ education.display = function()
 		 { 
 			var formattedonlineCourseTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
 			var formattedonlineCourseSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
-			var formattedonlineCoursesDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates +"<br>");
-			var formattedonlinecoursesURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+			//var formattedonlineCoursesDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates +"<br>");
+			formattedonlineCourseTitle = formattedonlineCourseTitle.replace("%url%", education.onlineCourses[course].url);
 
 			$(".education-entry:last").append(formattedonlineCourseTitle + formattedonlineCourseSchool);
-			$(".education-entry:last").append(formattedonlineCoursesDates);
-			$(".education-entry:last").append(formattedonlinecoursesURL)
+			//$(".education-entry:last").append(formattedonlineCoursesDates);
+			//$(".education-entry:last").append(formattedonlinecoursesURL)
 		 }
 	}
 }
 
-window.addEventListener('resize', refresh);
+window.addEventListener('resize', refresh, true);  //Third argument needed to prevent Firefox exception
 
 function refresh()
 {
